b46fbd4d1ef9b4512e8b372c0e2b8cf8979b7032
bhuvanesh245 <bhuvaneshgudipalli366.com>
2025-01-08 14:06:51 +0530

    B and C Conversation

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Bhuvan
+PERSON2=Charan
+
+echo    "$PERSON1::     Hi $PERSON2"
+echo    "$PERSON2::    Hello $PERSON1"
+echo    "$PERSON1::     How are you?"
+echo    "$PERSON2::    I am fine.Tq"
+echo    "$PERSON1::    ok"
