40fd41e05e886abcfb182cfddf4335b5d815c143
bhuvanesh245 <bhuvaneshgudipalli366.com>
2025-01-14 10:37:27 +0530

    shell-script

--- 12-colours.sh
+++ 12-colours.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-echo -e "\e[30m BHUVANESH"
-echo -e "\e[31m BHUVANESH"
-echo -e "\e[32m BHUVANESH"
-echo -e "\e[33m BHUVANESH"
-echo -e "\e[34m BHUVANESH"
-echo -e "\e[35m BHUVANESH"
-echo -e "\e[36m BHUVANESH"
-echo -e "\e[37m BHUVANESH"
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="e[33m"
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else        
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: you must have sudo access"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+    else
+    echo -e "MySQL is already ... $Y INSTALLED"  
+    fi
+
+dnf list installed git
+
+    if [ $? -ne 0 ]
+    then
+        dnf install git -y
+        VALIDATE $? "Installing Git "
+else
+    echo -e "Git is already ...$Y INSTALLED"    
+fi
+
 
