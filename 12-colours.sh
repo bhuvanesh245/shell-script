6d182fc1595b8241684ed42809df57434c238cdc
bhuvanesh245 <bhuvaneshgudipalli366.com>
2025-01-14 10:19:20 +0530

    shell-script

---
+++ 12-colours.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "\e[30m BHUVANESH"
+echo -e "\e[31m BHUVANESH"
+echo -e "\e[32m BHUVANESH"
+echo -e "\e[33m BHUVANESH"
+echo -e "\e[34m BHUVANESH"
+echo -e "\e[35m BHUVANESH"
+echo -e "\e[36m BHUVANESH"
+echo -e "\e[37m BHUVANESH"
+
